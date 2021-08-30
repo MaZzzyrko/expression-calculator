2da35275644a081e1d2e12b2121d4d298f8983fd
Evgeny <hatehateme@bk.ru>
2021-08-30 12:15:24 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -3,9 +3,23 @@ function eval() {
     return;
 }
 
-function expressionCalculator(expr) {
+function expressionCalculator(x) {
     // write your solution here
-}
+    if (x.includes('/0') == true || x.includes('/ 0') == true) {
+        throw new Error("TypeError: Division by zero.")
+    
+    
+    }   else if ((x.match(/\(/g) || []).length == (x.match(/\) /g) || [ ]).length) {
+        return Function(`return (${x})`)()
+        
+    
+    
+    }   else {
+        throw new Error("ExpressionError: Brackets must be paired")
+
+    }
+  
+}  
 
 module.exports = {
     expressionCalculator
